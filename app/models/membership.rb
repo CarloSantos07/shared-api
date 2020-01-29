38a426809b1175c2ebc99fa1665e6d10bcf64447
ae074267 <C_Edson.Andrew@cerner.com>
2020-01-29 14:35:05 -0600

    Started models but I know they're not accurate - just initial thoughts on them - WIP

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,10 +1,19 @@
 class Membership < ApplicationRecord
   belongs_to :team, optional: true
-
   before_validation :set_uuid, on: :create
   validates :id, presence: true
+  default_scope { order(:name) }
 
   def set_uuid
     self.id = SecureRandom.uuid
+  end
+
+  def self.active
+    Member.where.not(memberships_id: nil).to_a
+  end
+
+  def teams
+    team_ids = MembershipTeam.where(member_id: google_user_id, active: true).map(&:teams_id).uniq
+    Team.find(team_ids)
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,38 @@
 class Team < ApplicationRecord
   has_many :memberships
-
   before_validation :set_uuid, on: :create
   validates :id, presence: true
+  default_scope { order(:name) }
 
   def set_uuid
     self.id = SecureRandom.uuid
+  end
+
+  def self.active
+    Team.where(active: true)
+  end
+
+  def members
+    membership_ids = MemberTeam.where(teams_id: id, active: true)
+  end
+
+  def add_member(member_id)
+    lead_id = lead_id.to_i
+    return self if members.map(&:id).include?(member_id)
+
+    MemberTeam.create(member_id: member_id, teams_id: id, active: true)
+    self
+  end
+
+  def remove_member(member_id)
+    member_id = member_id.to_i
+    return self unless members.map(&:id).include?(member_id)
+
+    MemberTeam.find_by(member_id: member_id, teams_id: id, active: true).update(active: false)
+    self
+  end
+
+  after_initialize do
+    self.active ||= true if new_record?
   end
 end
